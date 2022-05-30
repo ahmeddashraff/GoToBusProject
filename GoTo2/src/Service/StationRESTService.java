333ca657dc7a86b6721e3379329abcc1466f7df0
ahmeddashraff <ahmeddashraf02@yahoo.com>
2022-05-30 23:54:02 +0200

    added StationREST class

---
+++ GoTo2/src/Service/StationRESTService.java
@@ -0,0 +1,39 @@
+package Service;
+
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response;
+
+
+import Entities.Station;
+import Managers.StationService;
+
+@Stateless
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("station")
+public class StationRESTService {
+	
+	@EJB
+	StationService stationService;
+
+	@POST
+	public Response CreateStation(Station station) {
+		ResponseBuilder builder;
+		if (station.getLatitude() > 90 || station.getLatitude() < -90 || station.getLongitude() > 180 || station.getLongitude() <-180)
+			throw new InternalServerErrorException();
+		else {
+			stationService.addStation(station);
+			builder = Response.ok();
+			return builder.build();
+		}
+	}
+}
