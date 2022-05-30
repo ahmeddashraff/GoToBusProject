dcdaeef1b05f77a26a7936d358740b16ca14c9e1
ahmoudBadr205 <mahmodbadr42@gmail.com>
2022-05-30 23:52:02 +0200

    added stationService class

---
+++ GoTo2/src/Managers/StationService.java
@@ -0,0 +1,25 @@
+package Managers;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import Entities.Station;
+
+@Stateless
+public class StationService {
+
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void addStation(Station station) {
+		
+		em.persist(station);
+	}
+
+	public void updateUser(Station station) {
+		em.merge(station);
+		
+	}
+}
