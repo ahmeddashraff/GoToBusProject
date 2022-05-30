4e7a0d22d7ad4402c430589fc3f281ff16d0ee6a
MahmouDMustafa60331 <mahmoud.mustafa.60331@gmail.com>
2022-05-30 23:50:01 +0200

    added station entity

---
+++ GoTo2/src/Entities/Station.java
@@ -0,0 +1,57 @@
+package Entities;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Station
+ *
+ */
+@Entity
+
+public class Station implements Serializable {
+
+	   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer station_id;
+	private String name;
+	private Double longitude;
+	private Double latitude;
+	private static final long serialVersionUID = 1L;
+
+	public Station() {
+		super();
+	}   
+	public Integer getStation_id() {
+		return this.station_id;
+	}
+
+	public void setStation_id(Integer station_id) {
+		this.station_id = station_id;
+	}   
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}   
+	public Double getLongitude() {
+		return this.longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}   
+	public Double getLatitude() {
+		return this.latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+   
+}
