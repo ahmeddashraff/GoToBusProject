23b09467006a46da729ac584a274e861b8ab9bd3
student <student@localhost>
2022-05-30 23:14:19 +0200

    added user service class

---
+++ GoTo2/src/Managers/UserService.java
@@ -0,0 +1,40 @@
+package Managers;
+
+
+import javax.ejb.Stateless;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import java.util.List;
+
+import Entities.User;
+
+@Stateless
+public class UserService {
+	
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public void addUser(User user) {
+		
+		em.persist(user);
+	}
+
+	public void updateUser(User user) {
+		em.merge(user);
+		
+	}
+	
+	public List<User> getAllUsers() {
+        TypedQuery<User> query = em.createNamedQuery("findAllUsers", User.class);
+        return query.getResultList();
+    }
+	
+	/*public User findUserbyid (int id)
+    {
+        return em.find(User.class, id);
+    }*/
+}
